6ff806e01f9a7c547f8fa99da3d4207fba227baa
Austin Zheng <austinzheng@gmail.com>
2015-05-10 16:32:17 -0700

    Adding unit tests
    Changes:
    - Added unit tests for '.read-string' and '.re-first' built-in fns

--- LambdatronTests/Builtins/TestReFirst.swift
+++ LambdatronTests/Builtins/TestReFirst.swift
@@ -8,4 +8,73 @@
 
 import Foundation
 
-// TODO
+/// Test the '.re-first' built-in function.
+class TestReFirstBuiltin : InterpreterTest {
+
+  /// .re-first should work with literal patterns containing no regex logic.
+  func testLiteralPattern() {
+    expectThat("(.re-first #\"ee\" \"meela\")", shouldEvalTo: .StringAtom("ee"))
+    expectThat("(.re-first #\"ee\" \"jyaku\")", shouldEvalTo: .Nil)
+    expectThat("(.re-first #\"s\" \"mississippi\")", shouldEvalTo: .StringAtom("s"))
+    expectThat("(.re-first #\"s\" \"indiana\")", shouldEvalTo: .Nil)
+  }
+
+  /// .re-first should work with patterns containing regex logic.
+  func testAdvancedPattern() {
+    expectThat("(.re-first #\"\\d\\d\" \"a12b34c56\")", shouldEvalTo: .StringAtom("12"))
+    expectThat("(.re-first #\"\\d*\" \"foobar\")", shouldEvalTo: .StringAtom(""))
+    expectThat("(.re-first #\"\\d+\" \"foobar\")", shouldEvalTo: .Nil)
+    expectThat("(.re-first #\"[a-z]+\" \"3.1415nine2six5three\")", shouldEvalTo: .StringAtom("nine"))
+    expectThat("(.re-first #\"[a-z]+\" \"3.141592653\")", shouldEvalTo: .Nil)
+  }
+
+  /// .re-first should work with patterns defining capture groups.
+  func testCaptureGroupsPattern() {
+    expectThat("(.re-first #\"\\d(\\d)\" \"a12b34c56\")",
+      shouldEvalTo: vectorWithItems(.StringAtom("12"), .StringAtom("2")))
+    expectThat("(.re-first #\"(\\d*)\" \"foobar\")",
+      shouldEvalTo: vectorWithItems(.StringAtom(""), .StringAtom("")))
+    expectThat("(.re-first #\"(\\d+)\" \"foobar\")", shouldEvalTo: .Nil)
+    expectThat("(.re-first #\"([a-z]+)\" \"3.1415nine2six5three\")",
+      shouldEvalTo: vectorWithItems(.StringAtom("nine"), .StringAtom("nine")))
+    expectThat("(.re-first #\"th([a-z]+)\" \"3.1415nine2six5three\")",
+      shouldEvalTo: vectorWithItems(.StringAtom("three"), .StringAtom("ree")))
+  }
+
+  /// .re-first should reject pattern arguments that aren't regex patterns.
+  func testInvalidPatterns() {
+    expectInvalidArgumentErrorFrom("(.re-first nil \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first 152 \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first true \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first false \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first \\a \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first :a \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first 'a \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first \"foo\" \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first [#\"foo\" #\"bar\"] \"foobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first .re-first \"foobar\")")
+  }
+
+  /// .re-first should reject string arguments that aren't strings.
+  func testInvalidStrings() {
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" nil)")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" true)")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" false)")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" 592)")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" -0.92871)")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" \\h)")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" :foo5092bar)")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" 'foo68172bar)")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" #\"124f52oobar\")")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" '())")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" [])")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" {})")
+    expectInvalidArgumentErrorFrom("(.re-first #\"\\d+\" .re-first)")
+  }
+
+  /// .re-first should take exactly two arguments.
+  func testArity() {
+    expectArityErrorFrom("(.re-first #\"ee\")")
+    expectArityErrorFrom("(.re-first #\"ee\" \"meela\" \"jyaku\")")
+  }
+}
--- LambdatronTests/Builtins/TestReadString.swift
+++ LambdatronTests/Builtins/TestReadString.swift
@@ -8,4 +8,118 @@
 
 import Foundation
 
-// TODO
+/// Test the '.read-string' built-in function.
+class TestReadStringBuiltin : InterpreterTest {
+
+  private func expectInputToReadString(input: String, toEvalTo result: ConsValue) {
+    return expectThat("(.read-string \"\(input)\")", shouldEvalTo: result)
+  }
+
+  /// .read-string should properly read in literal keywords.
+  func testLiteralKeywords() {
+    expectInputToReadString("nil", toEvalTo: .Nil)
+    expectInputToReadString("true", toEvalTo: true)
+    expectInputToReadString("false", toEvalTo: false)
+  }
+
+  /// .read-string should properly read in numbers.
+  func testWithNumbers() {
+    expectInputToReadString("0", toEvalTo: 0)
+    expectInputToReadString("3.141592", toEvalTo: 3.141592)
+    expectInputToReadString("-928187859291", toEvalTo: -928187859291)
+  }
+
+  /// .read-string should properly read in symbols.
+  func testWithSymbols() {
+    let fooSymbol = symbol("foo")
+    let barSymbol = symbol("bar")
+    let fqFoo = symbol("foo", namespace: "meela")
+    expectInputToReadString("foo", toEvalTo: .Symbol(fooSymbol))
+    expectInputToReadString("bar", toEvalTo: .Symbol(barSymbol))
+    expectInputToReadString("meela/foo", toEvalTo: .Symbol(fqFoo))
+  }
+
+  /// .read-string should properly read in keywords.
+  func testWithKeywords() {
+    let fooKeyword = keyword("foo")
+    let barKeyword = keyword("bar")
+    let fqFoo = keyword("foo", namespace: "meela")
+    expectInputToReadString(":foo", toEvalTo: .Keyword(fooKeyword))
+    expectInputToReadString(":bar", toEvalTo: .Keyword(barKeyword))
+    expectInputToReadString(":meela/foo", toEvalTo: .Keyword(fqFoo))
+  }
+
+  /// .read-string should properly read in character literals.
+  func testWithCharacters() {
+    expectInputToReadString("\\\\a", toEvalTo: .CharAtom("a"))
+    expectInputToReadString("\\\\newline", toEvalTo: .CharAtom("\n"))
+  }
+
+  /// .read-string should properly read in strings.
+  func testWithStrings() {
+    expectInputToReadString("\\\"\\\"", toEvalTo: .StringAtom(""))
+    expectInputToReadString("\\\"hello world\\\"", toEvalTo: .StringAtom("hello world"))
+  }
+
+  /// .read-string should properly read in lists.
+  func testWithLists() {
+    let fooSymbol = symbol("foo")
+    expectInputToReadString("()", toEvalTo: listWithItems())
+    expectInputToReadString("(1 2 3 4)", toEvalTo: listWithItems(1, 2, 3, 4))
+    expectInputToReadString("(foo [1 2] \\\"three\\\" 4)",
+      toEvalTo: listWithItems(.Symbol(fooSymbol), vectorWithItems(1, 2), .StringAtom("three"), 4))
+  }
+
+  /// .read-string should properly read in vectors.
+  func testWithVectors() {
+    expectInputToReadString("[]", toEvalTo: vectorWithItems())
+    expectInputToReadString("[[[1 2] 3] [4 [5] [6]] 7]", toEvalTo:
+      vectorWithItems(
+        vectorWithItems(
+          vectorWithItems(1, 2),
+          3),
+        vectorWithItems(4,
+          vectorWithItems(ConsValue.IntAtom(5)),
+          vectorWithItems(ConsValue.IntAtom(6))),
+        7))
+  }
+
+  /// .read-string should properly read in maps.
+  func testWithMaps() {
+    expectInputToReadString("{}", toEvalTo: mapWithItems())
+    expectInputToReadString("{1 2, 3 4}", toEvalTo: mapWithItems((1, 2), (3, 4)))
+    let foo : ConsValue = .Symbol(symbol("foo"))
+    let bar : ConsValue = .Keyword(keyword("bar"))
+    expectInputToReadString("{(10 20 foo) {:bar true nil false}}", toEvalTo:
+      mapWithItems((listWithItems(10, 20, foo), mapWithItems((bar, true), (.Nil, false)))))
+  }
+
+  /// .read-string should properly read in built-in functions.
+  func testWithBuiltInFunctions() {
+    expectInputToReadString(".cons", toEvalTo: .BuiltInFunction(.Cons))
+    expectInputToReadString(".read-string", toEvalTo: .BuiltInFunction(.ReadString))
+  }
+
+  /// .read-string should reject non-string arguments.
+  func testNonStringArguments() {
+    expectInvalidArgumentErrorFrom("(.read-string nil)")
+    expectInvalidArgumentErrorFrom("(.read-string true)")
+    expectInvalidArgumentErrorFrom("(.read-string false)")
+    expectInvalidArgumentErrorFrom("(.read-string -9281)")
+    expectInvalidArgumentErrorFrom("(.read-string 69912.123123)")
+    expectInvalidArgumentErrorFrom("(.read-string :y)")
+    expectInvalidArgumentErrorFrom("(.read-string 'y)")
+    expectInvalidArgumentErrorFrom("(.read-string \\y)")
+    expectInvalidArgumentErrorFrom("(.read-string #\"[0-9]+\")")
+    expectInvalidArgumentErrorFrom("(.read-string ())")
+    expectInvalidArgumentErrorFrom("(.read-string [])")
+    expectInvalidArgumentErrorFrom("(.read-string {})")
+    expectInvalidArgumentErrorFrom("(.read-string .read-string)")
+  }
+
+  /// .read-string should take exactly one argument.
+  func testArity() {
+    expectArityErrorFrom("(.read-string)")
+    expectArityErrorFrom("(.read-string \"hello\" \"world\")")
+  }
+}
